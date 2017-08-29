457ba873368ff047edc38e064356b6f6506816e0
Nikola Chachev <otss@abv.bg>
2017-08-29 07:20:34 +0300

    Update MyFunc.cpp

--- MyFunc.cpp
+++ MyFunc.cpp
@@ -1,4 +1,4 @@
-#include "MyFunc.h"
+#include "Functions.h"
 int mystrlen(char* src)
 {
     int counter = 0;
@@ -13,4 +13,14 @@ void mystrcpy(char* dst, char* src)
     for(int c = 0; c < mystrlen(src) + 1;c++)
         dst[c] = src[c];
 }
-
+bool mystrcmp(char* src1, char* src2)
+{
+    if(mystrlen(src1) != mystrlen(src2))
+        return false;
+    for(int c = 0; c < mystrlen(src1); c++)
+    {
+        if(src1[c] != src2[c])
+            return false;
+    }
+    return true;
+}
