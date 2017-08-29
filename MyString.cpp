752822bb5c1a4345ef575fff190684734a84a973
Nikola Chachev <otss@abv.bg>
2017-08-29 07:17:19 +0300

    Update MyString.cpp

--- MyString.cpp
+++ MyString.cpp
@@ -1,15 +1,13 @@
-#include "MyString.h"
+#include "String.h"
 void String::copy(const String& other)
 {
-    delete[] this->data;
-    this->SIZE = other.SIZE;
+    this->SIZE = other.SIZE ;
     this->data = new char[SIZE];
     mystrcpy(this->data,other.data);
 }
 void String::destroy()
 {
     delete[] this->data;
-    this->SIZE = 0;
 }
 String::String()
 {
@@ -18,7 +16,7 @@ String::String()
 }
 String::String(char* data)
 {
-    this->SIZE = mystrlen(data);
+    this->SIZE = mystrlen(data) + 1;
     this->data = new char[SIZE];
     mystrcpy(this->data,data);
 }
@@ -35,33 +33,17 @@ String& String::operator=(const String& other)
     }
     return *this;
 }
+String& String::operator=(char* obj)
+{
+    destroy();
+    this->SIZE = mystrlen(obj);
+    this->data = new char[SIZE];
+    mystrcpy(data,obj);
+    return *this;
+}
 String::~String()
 {
     destroy();
-}
-void String::newsize(int newsize)
-{
-    if(this->SIZE > 0)
-    {
-        char* temp;
-        temp = new char[this->SIZE];
-        temp = this->data;
-        delete[] data;
-        this->SIZE = newsize;
-        this->data = new char[SIZE];
-        mystrcpy(this->data,temp);
-        delete[] temp;
-    }
-    else
-    {
-        delete[] data;
-        this->SIZE = newsize;
-        data = new char[SIZE];
-    }
-}
-void String::SetSize(int newSize)
-{
-    newsize(newSize);
 }
 int String::GetSize() const
 {
@@ -71,27 +53,27 @@ char* String::GetData() const
 {
     return this->data;
 }
-char String::GetAt(int index)
-{
-    return this->data[index];
-}
 void String::SetAt(int index, char symbol)
 {
-    this->data[index] = symbol;
+   if(index >= 0 && index <= this->SIZE)
+        this->data[index] = symbol;
+   else
+    cout << "error index out of bounds" << endl;
 }
 char* String::operator+(const String& two)
 {
     int size1 = mystrlen(this->data);
     int size2 = mystrlen(two.GetData());
-    char* temp = new char[size1 + size2];
+    char* temp = new char[size1 + size2 + 1];
     for(int i = 0; i< size1; ++i)
     {
         temp[i] = this->data[i];
     }
     for(int i = 0; i< size2; ++i)
     {
-        temp[i + size1] = two[i];
+        temp[i + size1 + 1] = two[i];
     }
+    cout << mystrlen(temp) << endl;
     temp[size1+size2] = '\0';
     return temp;
 }
@@ -106,14 +88,18 @@ void String::AddChar(char ch)
     else
     {
         this->SIZE += 1;
-        data[SIZE - 1] = ch;
+        char* temp = new char[this->SIZE];
+        mystrcpy(temp,this->data);
+        this->data = new char[this->SIZE];
+        mystrcpy(this->data,temp);
+        this->data[this->SIZE - 1] = ch;
     }
 }
 char& String::operator[](int index)
 {
     return data[index];
 }
-char& String::operator[](int index) const
+char String::operator[](int index) const
 {
     if(index < 0 || index > SIZE)
         cout << "wrong input" << endl;
@@ -121,39 +107,19 @@ char& String::operator[](int index) const
 }
 bool String::operator<(const String& other)
 {
-    bool flag;
-    if(this->SIZE< other.SIZE)
-        flag = true;
-    else
-        flag = false;
-    return flag;
+    return (this->SIZE< other.SIZE);
 }
 bool String::operator>(const String& other)
 {
-    bool flag;
-    if(this->SIZE > other.SIZE)
-        flag = true;
-    else
-        flag = false;
-    return flag;
+    return(this->SIZE > other.SIZE);
 }
 bool String::operator<=(const String& other)
 {
-    bool flag;
-    if(this->SIZE<=other.SIZE)
-        flag = true;
-    else
-        flag = false;
-    return flag;
+    return !operator>(other);
 }
 bool String::operator>=(const String& other)
 {
-    bool flag;
-    if(this->SIZE >=other.SIZE)
-        flag = true;
-    else
-        flag = false;
-    return flag;
+    return !operator<(other);
 }
 bool String::operator==(const String& other)
 {
@@ -164,7 +130,7 @@ bool String::operator==(const String& other)
     {
         for(int c = 0;c <this->SIZE;c++)
         {
-            if(GetAt(c) != other[c])
+            if(this->data[c] != other[c])
                 return flag = false;
         }
     }
@@ -179,7 +145,7 @@ bool String::operator!=(const String& other)
     {
         for(int c =0;c<this->SIZE;c++)
         {
-            if(GetAt(c) != other[c])
+            if(this->data[c] != other[c])
                 return flag = true;
         }
     }
@@ -189,7 +155,7 @@ String& String::operator+=(const String& other)
 {
     int size1 = mystrlen(this->data);
     int size2 = mystrlen(other.data);
-    char* temp = new char[size1 + size2];
+    char* temp = new char[size1 + size2 + 1];
     for(int i = 0; i< size1; ++i)
     {
         temp[i] = this->data[i];
@@ -198,25 +164,28 @@ String& String::operator+=(const String& other)
     {
         temp[i + size1] = other[i];
     }
-    temp[size1+size2] = '\0';
-    copy(temp);
+    this->SIZE = mystrlen(temp);
+    this->data = new char[this->SIZE];
+    mystrcpy(this->data,temp);
+    delete[] temp;
     return *this;
 }
 istream& operator>>(istream& is, String& src)
 {
-    src.~String();
+    src.destroy();
     String temp;
     char ch = NULL;
     while(is.get(ch) && ch != '\n')
         temp.AddChar(ch);
-    src.SetSize(temp.GetSize());
-    for(unsigned int c = 0; c < temp.GetSize(); c++)
+    src.SIZE = temp.SIZE ;
+    src.data = new char[src.SIZE + 1];
+    for(unsigned int c = 0; c < temp.SIZE; c++)
         src.SetAt(c,temp[c]);
+    src.data[src.SIZE] = '\0';
     return is;
 }
 ostream& operator<<(ostream& os, String& src)
 {
-    for(int c = 0; c < src.SIZE; c++)
-        os << src[c];
+    os << src.data;
     return os;
 }
